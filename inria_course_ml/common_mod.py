99bf1f1295557e3063d9912cf0245631a3f9bb14
pedroATdidekind <pedro@didekin.es>
2025-11-16 20:26:57 +0100

    Closing module 2

--- inria_course_ml/common_mod.py
+++ inria_course_ml/common_mod.py
@@ -6,12 +6,20 @@ import scipy as sc
 data_dir_path = os.path.abspath('../')
 
 
-def dfFromArffFile(strpath):
+def filePath(strpath):
     file_path = data_dir_path + strpath
     assert os.path.exists(file_path)
-    arff_file = sc.io.arff.loadarff(file_path)
+    return file_path
+
+
+def dfFromArffFile(strpath):
+    arff_file = sc.io.arff.loadarff(filePath(strpath))
     print(print(f'Metadata =============================================\n {arff_file[1]}'))
     df = pd.DataFrame(arff_file[0])
     for column in df.select_dtypes(include='object').columns:
         df[column] = df[column].str.decode('utf-8')
     return df
+
+
+def dfFromCsvFile(strpath):
+    return pd.read_csv(filePath(strpath))
